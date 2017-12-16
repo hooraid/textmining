8058e66ace0e3cfde8ac75809a0e5615fdbbd53a
starriest <starriest30@gmail.com>
2017-12-16 22:40:33 +0900

    1216

--- clustering/ex_lda.py
+++ clustering/ex_lda.py
@@ -3,11 +3,29 @@ import pickle
 import operator
 lda = LDA()
 
+# make model (모델생성용! 사용안할 시 주석처리)
+lda = LDA()
+topic_n = 10 # 토픽 개수
+lda.makemodel(topic_n)
 
-# make model (run only first try)
-lda.makemodel()
 lda.load()
 
+f = open("./data/preprocessed_lyric222.txt","r",encoding="utf-8")
+doc = {}
+
+for line in f:
+    id_song = line.split(";")
+    document_topics, word_topic, word_phi = lda.getTopic(lda.sen2bow(id_song[1]))
+    doc[id_song[0]]= document_topics
+f.close()
+
+f2 = open("./data/ldamatrix.txt", "wb")
+
+pickle.dump(doc, f2)
+
+f2.close()
+
+# !!for test!!
 # get dense LDA matrix
 LDAmatrix = pickle.load(open("./data/ldamatrix.txt","rb"))
 
